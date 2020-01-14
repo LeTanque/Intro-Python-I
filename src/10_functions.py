e76ce04742e92b082a773b5d2bc53343e0ff29b6
Frank Martinez <frankie.martinez@gmail.com>
2020-01-14 14:40:16 -0800

    ✨ functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -3,10 +3,30 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+userInput = input("Enter a number: ")
+# num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
 
+# YOUR CODE HERE
+def is_even(hot_fiyah):
+    # First lets do type checking for number, string, float
+    try:
+        val = int(hot_fiyah)
+    except ValueError:
+        try:
+            val = float(hot_fiyah)
+            return print("Itsa floaty! No a likey a stringys!", val)
+        except ValueError:
+            return print("Itsa stringy!")
+    # Now, in order to get to this point we must have a number.
+    # Let's check to see if it's divisible by 2. Trigger the
+    # appropriate response.
+    if val % 2 == 0:
+        print("\nEven!\n")
+    else:
+        print("\nOdd!\n")
+
+
+is_even(userInput)
